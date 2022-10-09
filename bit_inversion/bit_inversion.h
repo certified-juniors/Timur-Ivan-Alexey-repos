b92fbf5d0df23184641b33a4fe65379fc0c7f7d6
Alexey <ax.chinaev@yandex.ru>
2022-10-09 20:18:50 +0000

    create sample tests

---
+++ bit_inversion/bit_inversion.h
@@ -0,0 +1,4 @@
+#include <iostream>
+
+unsigned int invert(unsigned int N, size_t K);
+void run(std::istream &, std::ostream &);
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "bit_inversion.h"
+
+int main() {
+    run(std::cin, std::cout);
+    return 0;
+}
