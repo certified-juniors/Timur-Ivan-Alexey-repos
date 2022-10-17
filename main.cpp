0b9cf73d55e0d501324862bd87d0d07eb50d2a0f
Vanvik <ivanspecial5@gmail.com>
2022-10-17 21:01:34 +0000

    final

--- main.cpp
+++ main.cpp
@@ -4,11 +4,8 @@
 
 int main(){
     int arr_size = 10;
-    std :: cout << " 1 hello from main" << std :: endl ;
     memoryLost(arr_size);
-    std :: cout << " 2 hello from main" << std :: endl ;
     extraIndex(arr_size);
-    std :: cout << " 3 hello from main" << std :: endl ;
 
     return 0;
 }
