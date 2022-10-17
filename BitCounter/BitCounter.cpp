6d6220d5696d702b9a529bcd644f000576cbdae3
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2022-10-17 18:38:15 +0000

    testing git-clang-format

--- BitCounter/BitCounter.cpp
+++ BitCounter/BitCounter.cpp
@@ -3,16 +3,16 @@
 int CountBits(unsigned number) {
   int count;
   while (number > 0) {
-    if (number & 1) {  // проверка на то, что последний бит единичный
+    if (number & 1) { // проверка на то, что последний бит единичный
       count++;
     }
-    number >>= 2;  // сдвигаем число на 2 бита вправо
+    number >>= 2; // сдвигаем число на 2 бита вправо
   }
   return count;
 }
 
 void run(std::istream &input, std::ostream &output) {
-    unsigned number;
-    input >> number;
-    output << CountBits(number) << std::endl;
+  unsigned number;
+  input >> number;
+  output << CountBits(number) << std::endl;
 }
